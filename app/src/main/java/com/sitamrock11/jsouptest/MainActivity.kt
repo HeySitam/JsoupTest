b9b73f0a6c71d5e9465ae46f573462e8feff7264
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-06-10 21:09:43 +0530

    firebase website checked

--- app/src/main/java/com/sitamrock11/jsouptest/MainActivity.kt
+++ app/src/main/java/com/sitamrock11/jsouptest/MainActivity.kt
@@ -2,10 +2,28 @@ package com.sitamrock11.jsouptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var imgTest: ImageView
+    override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imgTest = findViewById(R.id.imgTest)
+        val url = "https://firebase.google.com/"
+        CoroutineScope(Dispatchers.IO).launch {
+            val document = Jsoup.connect(url).get()
+            val img = document.select("img").first()
+            val imgSrc = img.absUrl("src")
+            withContext(Dispatchers.Main){
+                Glide.with(baseContext).load(imgSrc).into(imgTest)
+            }
+        }
     }
 }
